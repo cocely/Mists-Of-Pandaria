0464a229de3aba017e134dbdace6902bc7460734
Easelm <easelm@live.com>
2013-08-04 01:17:30 -0400

    UPDATED
    Updated the auth database base sql file. This incldues the realmlist sql structure and account structure. Added realm_races and realm_classes
    
    ADDED: Mists of Pandaria folder in the sql directory. realm_classes and realm_races is in a separated folder under 'auth' inside of Mists of Pandaria directory.

--- sql/base/auth_database.sql
+++ sql/base/auth_database.sql
@@ -37,7 +37,7 @@ CREATE TABLE `account` (
   `lock_country` varchar(2) NOT NULL DEFAULT '00',
   `last_login` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   `online` tinyint(3) unsigned NOT NULL DEFAULT '0',
-  `expansion` tinyint(3) unsigned NOT NULL DEFAULT '3',
+  `expansion` tinyint(3) unsigned NOT NULL DEFAULT '4',
   `mutetime` bigint(20) NOT NULL DEFAULT '0',
   `mutereason` varchar(255) NOT NULL DEFAULT '',
   `muteby` varchar(50) NOT NULL DEFAULT '',
@@ -479,7 +479,7 @@ CREATE TABLE `realmlist` (
   `timezone` tinyint(3) unsigned NOT NULL DEFAULT '0',
   `allowedSecurityLevel` tinyint(3) unsigned NOT NULL DEFAULT '0',
   `population` float unsigned NOT NULL DEFAULT '0',
-  `gamebuild` int(10) unsigned NOT NULL DEFAULT '15595',
+  `gamebuild` int(10) unsigned NOT NULL DEFAULT '17128',
   PRIMARY KEY (`id`),
   UNIQUE KEY `idx_name` (`name`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='Realm System';
@@ -511,6 +511,62 @@ CREATE TABLE `uptime` (
   PRIMARY KEY (`realmid`,`starttime`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Uptime system';
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+-- ----------------------------
+-- Table structure for `realm_classes`
+-- ----------------------------
+DROP TABLE IF EXISTS `realm_classes`;
+CREATE TABLE `realm_classes` (
+  `realmId` int(11) NOT NULL,
+  `class` tinyint(4) NOT NULL COMMENT 'Class Id',
+  `expansion` tinyint(4) NOT NULL COMMENT 'Expansion for class activation',
+  PRIMARY KEY (`realmId`,`class`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of realm_classes
+-- ----------------------------
+INSERT INTO `realm_classes` VALUES ('1', '1', '0');
+INSERT INTO `realm_classes` VALUES ('1', '2', '0');
+INSERT INTO `realm_classes` VALUES ('1', '3', '0');
+INSERT INTO `realm_classes` VALUES ('1', '4', '0');
+INSERT INTO `realm_classes` VALUES ('1', '5', '0');
+INSERT INTO `realm_classes` VALUES ('1', '6', '2');
+INSERT INTO `realm_classes` VALUES ('1', '7', '0');
+INSERT INTO `realm_classes` VALUES ('1', '8', '0');
+INSERT INTO `realm_classes` VALUES ('1', '9', '0');
+INSERT INTO `realm_classes` VALUES ('1', '10', '4');
+INSERT INTO `realm_classes` VALUES ('1', '11', '0');
+
+-- ----------------------------
+-- Table structure for `realm_races`
+-- ----------------------------
+DROP TABLE IF EXISTS `realm_races`;
+CREATE TABLE `realm_races` (
+  `realmId` int(11) NOT NULL,
+  `race` tinyint(4) NOT NULL COMMENT 'Race Id',
+  `expansion` tinyint(4) NOT NULL COMMENT 'Expansion for race activation',
+  PRIMARY KEY (`realmId`,`race`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of realm_races
+-- ----------------------------
+INSERT INTO `realm_races` VALUES ('1', '1', '0');
+INSERT INTO `realm_races` VALUES ('1', '2', '0');
+INSERT INTO `realm_races` VALUES ('1', '3', '0');
+INSERT INTO `realm_races` VALUES ('1', '4', '0');
+INSERT INTO `realm_races` VALUES ('1', '5', '0');
+INSERT INTO `realm_races` VALUES ('1', '6', '0');
+INSERT INTO `realm_races` VALUES ('1', '7', '0');
+INSERT INTO `realm_races` VALUES ('1', '8', '0');
+INSERT INTO `realm_races` VALUES ('1', '9', '3');
+INSERT INTO `realm_races` VALUES ('1', '10', '1');
+INSERT INTO `realm_races` VALUES ('1', '11', '1');
+INSERT INTO `realm_races` VALUES ('1', '22', '3');
+INSERT INTO `realm_races` VALUES ('1', '24', '4');
+INSERT INTO `realm_races` VALUES ('1', '25', '4');
+INSERT INTO `realm_races` VALUES ('1', '26', '4');
 
 --
 -- Dumping data for table `uptime`
